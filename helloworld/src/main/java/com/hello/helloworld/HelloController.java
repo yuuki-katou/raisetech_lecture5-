a69f494f124613d5c51d7ef0033f5da6a06a86a0
yuuki-katou <kayu.m365.enginner.blog@gamil.com>
2023-08-27 23:01:06 +0900

    HelloControllerに学生を検索する機能を追加

--- helloworld/src/main/java/com/hello/helloworld/HelloController.java
+++ helloworld/src/main/java/com/hello/helloworld/HelloController.java
@@ -1,20 +1,28 @@
 package com.hello.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class HelloController {
+    List<Student> students = List.of(
+            new Student("山田太郎", "xxx@example.co.jp", "java"),
+            new Student("橋本二郎", "xxo@example.co.jp", "java"),
+            new Student("小川三郎", "xoo@example.co.jp", "AWS"));
+
     @GetMapping("/hello")
-    public List<Student> hello() {
-
-        List<Student> students = List.of(
-                new Student("山田太郎", "xxx@example.co.jp", "java"),
-                new Student("橋本二郎", "xxo@example.co.jp", "java"),
-                new Student("小川三郎", "xoo@example.co.jp", "AWS"));
-
-        return students;
+    public List<Student>hello(@RequestParam(required = false) String name) {
+        if(name == null || name.isEmpty()){
+            return students;
+        }
+        return students.stream().filter(student -> student.getName().contains(name)).toList();
 
     }
+
 }
+
+
